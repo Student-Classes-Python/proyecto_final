cb33532143f1fdec18eee407ff38389386071030
HugoM2013 <huguitamon@gmail.com>
2024-04-21 10:31:19 +0200

    Data Model creado

---
+++ Models/data_model.py
@@ -0,0 +1,9 @@
+class DataModel():
+    def __init__(self, cantidad_clientes, popularidad, precio_entrada):
+        self.cantidad_clientes = cantidad_clientes
+        self.popularidad = popularidad
+        self.precio_entrada = precio_entrada
+
+    
+
+    
